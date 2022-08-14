3647108841851b564a77de09855567ca5ee2a0d9
Daniel Lincango <flincangoups@gmail.com>
2022-08-14 09:36:45 -0500

    Init anguar material app

---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [MatToolbarModule],
+})
+export class MaterialModule {}
