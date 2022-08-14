22dcd4dd61162461c9cba01d8c0afafa2e347e93
Daniel Lincango <flincangoups@gmail.com>
2022-08-14 12:58:42 -0500

    Init circular restriction form

--- src/app/pages/circulation/circulation.module.ts
+++ src/app/pages/circulation/circulation.module.ts
@@ -1,17 +1,18 @@
+import { MaterialModule } from './../../material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CirculationRoutingModule } from './circulation-routing.module';
 import { RestrictionComponent } from './component/restriction/restriction.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    RestrictionComponent
-  ],
+  declarations: [RestrictionComponent],
   imports: [
     CommonModule,
-    CirculationRoutingModule
-  ]
+    CirculationRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
-export class CirculationModule { }
+export class CirculationModule {}
--- src/app/pages/circulation/component/restriction/restriction.component.ts
+++ src/app/pages/circulation/component/restriction/restriction.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-restriction',
   templateUrl: './restriction.component.html',
-  styleUrls: ['./restriction.component.scss']
+  styleUrls: ['./restriction.component.scss'],
 })
 export class RestrictionComponent implements OnInit {
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
 
-  constructor() { }
+  registrationForm: FormGroup = this.fb.group({
+    plateId: ['', Validators.required],
+    evaluationDate: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onEvaluateRestriction() {}
 }
